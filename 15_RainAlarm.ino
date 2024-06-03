8e5c4b63d1753b7544f2b4d705e84ce8c47a6246
serkanguentin <guntinfurkan@gmail.com>
2024-06-03 09:26:25 +0200

    RainAlarm

--- 15_RainAlarm.ino
+++ 15_RainAlarm.ino
@@ -1,16 +1,17 @@
 int sensorPin = A0;                
-int esikDegeri = 250;             
+int minValue = 250;             
 int buzzerPin = 8;                 
-int veri;                          
+int data;                          
 
 void setup() {
   pinMode(buzzerPin, OUTPUT); 
   Serial.begin(9600);      
 }
 void loop() {
-  veri = analogRead(sensorPin);  
-  Serial.println(veri);delay(300); 
-  if(veri > esikDegeri){          
+  data = analogRead(sensorPin);  
+  Serial.println(data);// if we want to read the data, we can write here to specify the value
+  delay(300); 
+  if(data > minValue){          
     digitalWrite(buzzerPin, HIGH); 
     delay(100);
     digitalWrite(buzzerPin, LOW);
